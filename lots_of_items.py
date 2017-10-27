dfd02a0765f9e9595c572701ac81f264520319bf
dsychin <desmond@shiunyean.tk>
2017-10-27 17:26:48 +0100

    added basic data for all database tables

--- lots_of_items.py
+++ lots_of_items.py
@@ -15,3 +15,23 @@ for category in categories:
     data = Category(name=category)
     session.add(data)
     session.commit()
+
+data = User(username='dsychin', email='chin.sy.des@gmail.com')
+session.add(data)
+session.commit()
+
+
+class Items:
+    def __init__(self, name, description, category_id, created_by_id):
+        self.name = name
+        self.description = description
+        self.category_id = category_id
+        self.created_by_id = created_by_id
+
+items = [Items('GTX 1060', 'Mid-range GPU', 6, 1)]
+
+for item in items:
+    data = Item(name=item.name, description=item.description,
+                category_id=item.category_id, created_by_id=item.created_by_id)
+    session.add(data)
+    session.commit()
